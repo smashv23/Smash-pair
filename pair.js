c5c7de0d84f875d66e552e9b46ea02beadc65a7c
smashv23 <princerwanda2004@gmail.com>
2025-06-22 12:53:45 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -28,8 +28,8 @@ function randomMegaId(length = 6, numberLength = 4) {
 async function uploadCredsToMega(credsPath) {
     try {
         const storage = await new Storage({
-            email: 'sylivanusmomanyi@gmail.com', // Your Mega A/c Email Here
-            password: 'Sylivanus@42620143' // Your Mega A/c Password Here
+            email: 'mdeem054@gmail.com', // Your Mega A/c Email Here
+            password: 'shadrack11' // Your Mega A/c Password Here
         }).ready;
         console.log('Mega storage initialized.');
 
